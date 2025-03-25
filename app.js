3395b0370b00427818b3939dd5b481c7fc293fae
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-03-25 15:08:29 +0000

    Added a custom error

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const authorRouter = require("./routes/authorRouter");
 app.use("/authors", authorRouter);
 app.get("/", (req, res) => res.send("Hello , world!"));
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}`);
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,16 +1,17 @@
 const db = require("../db");
+const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../errors/CustomNotFound");
 
-async function getAuthorById(req, res) {
+const getAuthorById = asyncHandler(async (req, res) => {
   const { authorId } = req.params;
 
   const author = await db.getAuthorById(Number(authorId));
 
   if (!author) {
-    res.status(404).send("Author not found");
-    return;
+    throw new CustomNotFoundError("Author not found");
   }
 
   res.send(`Author Name: ${author.name}`);
-}
+});
 
 module.exports = { getAuthorById };
