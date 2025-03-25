4fb22a321927283aba6a26d1a90931970c70fcd9
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-03-25 14:16:29 +0000

    Added a route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const authorRouter = require("./routes/authorRouter");
 
+app.use("/authors", authorRouter);
 app.get("/", (req, res) => res.send("Hello , world!"));
 
 const PORT = 3000;
---
+++ routes/authorRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const authorRouter = Router();
+
+authorRouter.get("/", (req, res) => res.send("All authors"));
+authorRouter.get("/:authorId", (req, res) => {
+  const { authorId } = req.params;
+  res.send(`Author id: ${authorId}`);
+});
+
+module.exports = authorRouter;
